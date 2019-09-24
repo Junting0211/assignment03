f9998883f59d4739a6b27767cbbcdfd7ddd10ef8
mmanchen <mmanchen@u.rochester.edu>
2019-09-23 20:23:29 -0400

    Create islomzhan-work.php

---
+++ islomzhan-work.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title> Islomzhan Akhmedov </title>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<script src="js/menu-highlighter.js"></script>
+</head>
+<body class= "container">
+
+
+	<header>
+		<a href= "islomzhan-index.php">
+			<h1> Islomzhan Akhmedov </h1>
+			<img src= "images/islomzhan.jpg" alt="Islomzhan">
+		</a>
+	</header>
+	<?php include "inc/nav-islomzhan.php"; ?>
+	<?php include "inc/nav.php"; ?>
+
+	<aside>
+		<ul>
+			<li>Computer Science Major</li>
+			<li>Class of 2020</li>
+			<li>University of Rochester</li>
+		</ul>
+	</aside>
+
+
+	<main>
+        <h1> Work </h1> 
+		<p>Here is a list of the projects I have worked on. Toyzland.com - this is a website that sells toys online. I made this ecommerce website for my friend's store.
+LoyalFarm - it is a mobile app that lets people to buy their groceries online. I am now working on this project and soon it will be possible to use, hopefully, before the end of this year.
+SpaceShooter - two years ago, I made this 2D space game that even the spaceships were designed by me.
+AppleProducts - this is one of my best web projects, I designed and coded this website from scratch for my one class project. It is available by this link to view: https://my.toyzland.com</p>
+	</main>
+
+
+	<?php include "inc/scripts.php";?>
+	<footer> <strong> CSC 174: Advanced Front-end Web Design and Development </strong></footer>
+	
+
+</body></html>
