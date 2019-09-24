d13bae1c1f67a980a3ca21648cee9000a413dfa0
mmanchen <mmanchen@u.rochester.edu>
2019-09-23 20:23:27 -0400

    Create islomzhan-index.php

---
+++ islomzhan-index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title> Islomzhan Akhmedov </title>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<script src="js/menu-highlighter.js"></script>
+</head>
+<body class= "container">
+
+
+	<header>
+		<a href= "islomzhan-index.php">
+			<h1> Islomzhan Akhmedov </h1>
+			<img src= "images/islomzhan.jpg" alt="Islomzhan">
+		</a>
+	</header>
+	<?php include "inc/nav-islomzhan.php"; ?>
+	<?php include "inc/nav.php"; ?>
+
+	<aside>
+		<ul>
+			<li>Computer Science Major</li>
+			<li>Class of 2020</li>
+			<li>University of Rochester</li>
+		</ul>
+	</aside>
+
+
+	<main>
+		<p>I am a student at University of Rochester studying Computer Science. I will graduate the school in this December. Previously, I studied at North American University and there I worked as a part-time front web developer. I designed there the college's sports and IT websites. Recently, I started to work on a website for a toy store and I'm still working on the design and integrating ecommerce functionalities to it. Also, I am developing a mobile application for selling local farm goods. </p>
+	</main>
+
+
+	<?php include "inc/scripts.php";?>
+	<footer> <strong> CSC 174: Advanced Front-end Web Design and Development </strong></footer>
+	
+
+</body></html>
