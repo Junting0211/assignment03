e0cac8fbb8036f0058fe77cb59da8ef495e0267d
mmanchen <mmanchen@u.rochester.edu>
2019-09-23 20:23:25 -0400

    Create islomzhan-background.php

---
+++ islomzhan-background.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title> Islomzhan Akhmedov </title>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<script src="js/menu-highlighter.js"></script>
+</head>
+<body class= "container">
+
+
+	<header>
+		<a href= "islomzhan-index.php">
+			<h1> Islomzhan Akhmedov </h1>
+			<img src= "images/islomzhan.jpg" alt="Islomzhan">
+		</a>
+	</header>
+	<?php include "inc/nav-islomzhan.php"; ?>
+	<?php include "inc/nav.php"; ?>
+
+	<aside>
+		<ul>
+			<li>Computer Science Major</li>
+			<li>Class of 2020</li>
+			<li>University of Rochester</li>
+		</ul>
+	</aside>
+
+
+	<main>
+        <h1> Background </h1> 
+		<p>I am from the city of Osh in the Kyrgyz Republic, which is located in Central Asia. Upon my graduation of the high school I came to the United States. I’ve been to amazing places in Texas, Colorado, New York and California. Outside the US, I’ve been to Moscow, Russia and Tashkent, Uzbekistan. I speak Uzbek, Kyrgyz, Turkish, Russian, and Arabic along with English. Since 2017, I am studying Computer Science at University of Rochester.</p>
+	</main>
+
+
+	<?php include "inc/scripts.php";?>
+	<footer> <strong> CSC 174: Advanced Front-end Web Design and Development </strong></footer>
+	
+
+</body></html>
