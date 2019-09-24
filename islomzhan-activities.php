d0d57e5ae3076b9bbaeefee003f4125ebeb3921d
mmanchen <mmanchen@u.rochester.edu>
2019-09-23 20:23:23 -0400

    Create islomzhan-activities.php

---
+++ islomzhan-activities.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title> Islomzhan Akhmedov </title>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<script src="js/menu-highlighter.js"></script>
+</head>
+<body class= "container">
+
+
+	<header>
+		<a href= "islomzhan-index.php">
+			<h1> Islomzhan Akhmedov </h1>
+			<img src= "images/islomzhan.jpg" alt="Islomzhan">
+		</a>
+	</header>
+	<?php include "inc/nav-islomzhan.php"; ?>
+	<?php include "inc/nav.php"; ?>
+
+	<aside>
+		<ul>
+			<li>Computer Science Major</li>
+			<li>Class of 2020</li>
+			<li>University of Rochester</li>
+		</ul>
+	</aside>
+
+
+	<main>
+        <h1> Activities </h1> 
+		<p>There was a need for a web designer at the North American University, and I was able to start a part time job at their IT Department. I use this term - a web designer because nowadays most of the web developers out there actually use web building applications that run on various types of Content Management Services like WordPress, Drupal, and Joomla with few or no coding involved.</p>
+	</main>
+
+
+	<?php include "inc/scripts.php";?>
+	<footer> <strong> CSC 174: Advanced Front-end Web Design and Development </strong></footer>
+	
+
+</body></html>
