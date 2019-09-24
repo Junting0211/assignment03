78087617021aabb55a793b81de897e48840118ee
mmanchen <mmanchen@u.rochester.edu>
2019-09-23 20:22:51 -0400

    Create dewey-background.php

---
+++ dewey-background.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title> Dewey Bazirake </title>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<script src="js/menu-highlighter.js"></script>
+</head>
+<body class= "container">
+
+
+	<header>
+		<a href= "dewey-index.php">
+			<h1> Dewey Bazirake </h1>
+			<img src= "images/dewey.jpg" alt="Dewey">
+		</a>
+	</header>
+	<?php include "inc/nav-dewey.php"; ?>
+	<?php include "inc/nav.php"; ?>
+
+	<aside>
+		<ul>
+			<li>Computer Science Major</li>
+			<li>Class of 2020</li>
+			<li>University of Rochester</li>
+		</ul>
+	</aside>
+
+
+	<main>
+        <h1> Background </h1>
+		<p>I was born and raised in Uganda. Uganda is located in the heart of Africa. It is home to 42 million people and the population is growing at an average of 3.7% per year (as of 2018). That means you should all expect more Ugandan babies in the upcoming years. Uganda is blessed with abundant sun, freshwater & a rich cultural diversity of people. We are home to 56 traditional tribes which all have respective languages, dances, cultural clothing & values. Despite the diversity, the people all thrive in unison. Uganda is known for its rich wildlife (including lions, giraffes, buffalos, crocodiles, hippopotamuses, etc), rich bird diversity and fresh-water lakes including Lake Victoria, the largest lake in Africa - mother to River Nile that is shared with Tanzania & Kenya. Uganda is a peaceful country that is vibrant with opportunity for growth habiting a people that treasure and preserve nature.</p>
+	</main>
+
+
+	<?php include "inc/scripts.php";?>
+	<footer> <strong> CSC 174: Advanced Front-end Web Design and Development </strong></footer>
+	
+
+</body></html>
