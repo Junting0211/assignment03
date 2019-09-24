0faa537cf3c60b30e940a9aed41eb93fd9880c09
mmanchen <mmanchen@u.rochester.edu>
2019-09-23 20:23:11 -0400

    Create nav-dewey.php

---
+++ inc/nav-dewey.php
@@ -0,0 +1,16 @@
+	<nav class="sub-menus">
+	<ul>
+		<li>
+			<a href="dewey-index.php"> Me </a>
+		</li>
+		<li>
+			<a href="dewey-background.php">Background</a>
+		</li>
+		<li>
+			<a href="dewey-work.php">Work</a>
+		</li>
+		<li>
+			<a href="dewey-activities.php"> Activities </a>
+		</li>
+	</ul>	
+	</nav>
