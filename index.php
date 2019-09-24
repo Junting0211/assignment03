c4d3ae3660b7080d11b8beeb4bad9a7931cb0e3b
mmanchen <mmanchen@u.rochester.edu>
2019-09-23 20:23:21 -0400

    Update index.php

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title> Assignment 3: Information Architecture | Team Barcelona </title>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<script src="js/menu-highlighter.js"></script>
+</head>
+<body class= "container">
+
+
+	<header>
+		<a href= "index.php">
+			<h1> Interest in Computer Science  </h1>
+			<img src= "images/computer-science.jpg" alt="computer">
+			
+		</a>
+	</header>
+
+<?php include "inc/nav.php"; ?>
+
+	<aside>
+		<ul>
+			<li>Passionate about CS</li>
+			<li>University of Rochester Students</li>
+			<li>Senior Year</li>
+		</ul>
+	</aside>
+
+
+	<main>
+	<div class = "row">
+			<div class="column"><p>These three students, Islomzhan, Dewey and Jialin are University of Rochester students with interests in Computer Science. It is Jialin's minor and Dewey's and Islomzhan's major. They have very distinct backgrounds, but they all found a way of working in techonological projects they have been interested in. The 3 of them have come a long way since none of them were born in the US. It is apparent they are passionate about traveling as well.  </p> </div>
+			<div class="column"> <p>The most intriguing part about these students is the projects they have been part of. In Islomzhan's case he is currently working on the development of an app called LoyalFarm, which allows people to buy groceries online. Dewey had the opportunity to work with with MIRO AI, a sports focused Artifical Intelligence company, that focuses on image processing of athletes in marathons. Additionally, Jialin interned at an investment company, where she became profficient in dalaya analysis and technical tools.<p></div>
+
+	</div>
+	
+	</main>
+	
+	<?php include "inc/scripts.php";?>
+	<footer> <strong> CSC 174: Advanced Front-end Web Design and Development </strong></footer>
+	
+
+</body></html>
+
