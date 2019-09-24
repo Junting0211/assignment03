8e0d2db3370aa5a5ca06c0f5e32ee6cc8ead3778
mmanchen <mmanchen@u.rochester.edu>
2019-09-23 20:23:35 -0400

    Create jialin-index.php

---
+++ jialin-index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title> Jialin Huo </title>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<script src="js/menu-highlighter.js"></script>
+</head>
+<body class= "container">
+
+
+	<header>
+		<a href= "jialin-index.php">
+			<h1> Jialin Huo </h1>
+			<img src= "images/jialin.jpg" alt="Jialin">
+		</a>
+	</header>
+	<?php include "inc/nav-jialin.php"; ?>
+	<?php include "inc/nav.php"; ?>
+
+	<aside>
+		<ul>
+			<li>Computer Science Minor</li>
+			<li>Class of 2020</li>
+			<li>University of Rochester</li>
+		</ul>
+	</aside>
+
+
+	<main>
+		<p>As a fifth-year student at the University of Rochester, I am double majoring in Business Finance and Art History, with a minor in Computer Science. Due to computer science and coding’s popularity and its extensive use in all kinds of fields and the business world, I have decided to re-classify from Class of 2019 to Class of 2020 in order to complete this challenging but extremely useful minor and coding abilities. Until now, I have acquired coding experience including Python, R Studio, HTML, CSS and little knowledge of JavaScript Plugins and I am thrilled to learn more from both my professors and classmates. </p>
+	</main>
+
+
+	<?php include "inc/scripts.php";?>
+	<footer> <strong> CSC 174: Advanced Front-end Web Design and Development </strong></footer>
+	
+
+</body></html>
