436fad4583693d848cc736c55359ee2ec3633904
mmanchen <mmanchen@u.rochester.edu>
2019-09-23 20:23:15 -0400

    Create nav-jialin.php

---
+++ inc/nav-jialin.php
@@ -0,0 +1,16 @@
+<nav class="sub-menus">
+	<ul>
+		<li>
+			<a href="jialin-index.php"> Me </a>
+		</li>
+		<li>
+			<a href="jialin-background.php">Background</a>
+		</li>
+		<li>
+			<a href="jialin-work.php">Work</a>
+		</li>
+		<li>
+			<a href="jialin-activities.php"> Activities </a>
+		</li>
+	</ul>	
+	</nav>
