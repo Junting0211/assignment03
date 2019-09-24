7748adddc6651df4d089846445d328526640f89e
mmanchen <mmanchen@u.rochester.edu>
2019-09-23 20:23:13 -0400

    Create nav-islomzhan.php

---
+++ inc/nav-islomzhan.php
@@ -0,0 +1,16 @@
+<nav class="sub-menus">
+	<ul>
+		<li>
+			<a href="islomzhan-index.php"> Me </a>
+		</li>
+		<li>
+			<a href="islomzhan-background.php">Background</a>
+		</li>
+		<li>
+			<a href="islomzhan-work.php">Work</a>
+		</li>
+		<li>
+			<a href="islomzhan-activities.php"> Activities </a>
+		</li>
+	</ul>	
+	</nav>
