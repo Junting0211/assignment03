6cae26f7ae509181477772b9433eb39f997f32b9
mmanchen <mmanchen@u.rochester.edu>
2019-09-23 20:23:17 -0400

    Create nav.php

---
+++ inc/nav.php
@@ -0,0 +1,16 @@
+	<nav class= "main-menu">
+	<ul>
+		<li>
+			<a href="index.php"> Home </a>
+		</li>
+		<li>
+			<a href="dewey-index.php">Dewey</a>
+		</li>
+		<li>
+			<a href="islomzhan-index.php">Islomzhan</a>
+		</li>
+		<li>
+			<a href="jialin-index.php"> Jialin </a>
+		</li>
+	</ul>	
+	</nav>
