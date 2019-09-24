202be97d9ce36a2e4e329c6907a12fc661347940
mmanchen <mmanchen@u.rochester.edu>
2019-09-23 20:23:33 -0400

    Create jialin-background.php

---
+++ jialin-background.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title> Jialin Huo </title>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<script src="js/menu-highlighter.js"></script>
+</head>
+<body class= "container">
+
+
+	<header>
+		<a href= "jialin-index.php">
+			<h1> Jialin Huo </h1>
+			<img src= "images/jialin.jpg" alt="Jialin">
+		</a>
+	</header>
+	<?php include "inc/nav-jialin.php"; ?>
+	<?php include "inc/nav.php"; ?>
+
+	<aside>
+		<ul>
+			<li>Computer Science Minor</li>
+			<li>Class of 2020</li>
+			<li>University of Rochester</li>
+		</ul>
+	</aside>
+
+
+	<main>
+        <h1> Background </h1>
+		<p>My hometown is in Shanghai, China, but I have moved to San Francisco Bay Area since when I was thirteen years old. I studied at a Christian school in which I skipped my eighth grade and started from high school immediately. During my fantastic four years in high school, I have demonstrated strong interest in visual designs and various categories of art including calligraphy, painting, photography, and etc. With further curiosity, I have begun learning the technological instruments and software, particularly Adobe Photoshop and Blender. </p>
+	</main>
+
+
+	<?php include "inc/scripts.php";?>
+	<footer> <strong> CSC 174: Advanced Front-end Web Design and Development </strong></footer>
+	
+
+</body></html>
