e95ba86d1a62dfa2dffdca049aaa8d3943ffc405
mmanchen <mmanchen@u.rochester.edu>
2019-09-23 20:23:31 -0400

    Create jialin-activities.php

---
+++ jialin-activities.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title> Jialin Huo </title>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<script src="js/menu-highlighter.js"></script>
+</head>
+<body class= "container">
+
+
+	<header>
+		<a href= "jialin-index.php">
+			<h1> Jialin Huo </h1>
+			<img src= "images/jialin.jpg" alt="Jialin">
+		</a>
+	</header>
+	<?php include "inc/nav-jialin.php"; ?>
+	<?php include "inc/nav.php"; ?>
+
+	<aside>
+		<ul>
+			<li>Computer Science Minor</li>
+			<li>Class of 2020</li>
+			<li>University of Rochester</li>
+		</ul>
+	</aside>
+
+
+	<main>
+        <h1> Activities </h1>
+		<p> Outside of school works and internships, I spend my free time with my hobbies and moreover exploring my possibilities in different kinds of areas, including sports and music. I am particularly interested in playing tennis, swimming, skiing, and golf. During other times, I have dedicated my energy to philanthropy and charity activities in which I consider them significant and memorable. For six years, I have gone to Seattle annually and served as a volunteer to plan and hold fundraisers in order to help with the education of vision-impaired children; and the program has helped over fifty students to progress from elementary school through college. </p>
+	</main>
+
+
+	<?php include "inc/scripts.php";?>
+	<footer> <strong> CSC 174: Advanced Front-end Web Design and Development </strong></footer>
+	
+
+</body></html>
