4d0a5a45e3c65fa72fac87c86927b40780c99bf9
mmanchen <mmanchen@u.rochester.edu>
2019-09-23 20:23:37 -0400

    Create jialin-work.php

---
+++ jialin-work.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title> Jialin Huo </title>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<script src="js/menu-highlighter.js"></script>
+</head>
+<body class= "container">
+
+
+	<header>
+		<a href= "jialin-index.php">
+			<h1> Jialin Huo </h1>
+			<img src= "images/jialin.jpg" alt="Jialin">
+		</a>
+	</header>
+	<?php include "inc/nav-jialin.php"; ?>
+	<?php include "inc/nav.php"; ?>
+
+	<aside>
+		<ul>
+			<li>Computer Science Minor</li>
+			<li>Class of 2020</li>
+			<li>University of Rochester</li>
+		</ul>
+	</aside>
+
+
+	<main>
+        <h1> Work </h1>
+		<p>While spending my summer break back in Shanghai in the past few years, I have had three internships in which two internships were at consulting companies and the other one was at an asset management company. Through the extensive training and the intensive work environment in consulting company, I have become proficient in data analysis through the use of coding and technical tools, reporting current market benchmarks and trends, and constructing interactive and animated electronic reports for customer use according to their desired criteria. Additionally, during the investment banking internship, I performed due diligence, prepared valuation analysis and client presentations. I have participated in an acquisition deal and an Initial Public Offering in which the transaction values over thirty million dollars. </p>
+	</main>
+
+
+	<?php include "inc/scripts.php";?>
+	<footer> <strong> CSC 174: Advanced Front-end Web Design and Development </strong></footer>
+	
+
+</body></html>
