450d67229725479fac089a3093ff1fd2564be5a1
Xueying Chen <41633812+xchen74@users.noreply.github.com>
2019-09-28 15:59:28 -0400

    paragraph styling

--- index.php
+++ index.php
@@ -30,8 +30,8 @@
 
 	<main>
 	<div class = "row">
-			<div class="column"><p>These three students, Islomzhan, Dewey and Jialin are University of Rochester students with interests in Computer Science. It is Jialin's minor and Dewey's and Islomzhan's major. They have very distinct backgrounds, but they all found a way of working in techonological projects they have been interested in. The 3 of them have come a long way since none of them were born in the US. It is apparent they are passionate about traveling as well.  </p> </div>
-			<div class="column"> <p>The most intriguing part about these students is the projects they have been part of. In Islomzhan's case he is currently working on the development of an app called LoyalFarm, which allows people to buy groceries online. Dewey had the opportunity to work with with MIRO AI, a sports focused Artifical Intelligence company, that focuses on image processing of athletes in marathons. Additionally, Jialin interned at an investment company, where she became profficient in dalaya analysis and technical tools.<p></div>
+			<div class="column1"><p>These three students, Islomzhan, Dewey and Jialin are University of Rochester students with interests in Computer Science. It is Jialin's minor and Dewey's and Islomzhan's major. They have very distinct backgrounds, but they all found a way of working in techonological projects they have been interested in. The 3 of them have come a long way since none of them were born in the US. It is apparent they are passionate about traveling as well.  </p> </div>
+			<div class="column2"> <p>The most intriguing part about these students is the projects they have been part of. In Islomzhan's case he is currently working on the development of an app called LoyalFarm, which allows people to buy groceries online. Dewey had the opportunity to work with with MIRO AI, a sports focused Artifical Intelligence company, that focuses on image processing of athletes in marathons. Additionally, Jialin interned at an investment company, where she became profficient in dalaya analysis and technical tools.<p></div>
 
 	</div>
 	
