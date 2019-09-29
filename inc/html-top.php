1779e03527068bf91928f1e17a021fc5ab9fc247
Junting0211 <jchen126@u.rochester.edu>
2019-09-29 18:37:09 -0400

    change a little of css
    newest update by Junting Chen

--- inc/html-top.php
+++ inc/html-top.php
@@ -5,7 +5,7 @@
 	<head>
 		<meta charset="utf-8">
 		
-		<title>Team Quito </title>
+		<title>Team Quito</title>
 		<link rel="stylesheet" href="css/styles.css">
 		<script src="js/menu-highlighter.js"></script>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
